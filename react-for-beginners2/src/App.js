fad55d385bf4968d8cd4fb4ae6314f93e8262d79
mjtime <min021802@gmail.com>
2024-12-18 21:43:54 +0900

    [6.0~6.3]useEffect 기본 사용법

--- react-for-beginners2/src/App.js
+++ react-for-beginners2/src/App.js
@@ -1,7 +1,27 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev +1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  useEffect(()=>{console.log("I run only once")}, []);
+  useEffect(()=>{
+      console.log("I run when 'keyword' changes");
+  }, [keyword]);
+  useEffect(()=>{
+      console.log("I run when 'counter' changes");
+  }, [counter]);
+  useEffect(()=>{
+      console.log("I run when 'keyword & counter' changes");
+  }, [keyword, counter]);
+  
   return (
     <div>
-      <h1>Welcome back!</h1>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."></input>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click</button>
     </div>
   );
 }
