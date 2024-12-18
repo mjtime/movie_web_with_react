38dde9071fdd0c5478ea7b82f945ce0a9fe63fa2
mjtime <min021802@gmail.com>
2024-12-18 22:15:47 +0900

    [6.4] useEffect의 cleanup 사용법

--- react-for-beginners2/src/App.js
+++ react-for-beginners2/src/App.js
@@ -1,27 +1,20 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev +1);
-  const onChange = (event) => setKeyword(event.target.value);
+function Hello(){
+  useEffect(() => {
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  }, []);
+  return <h1>Hello</h1>;
+}
 
-  useEffect(()=>{console.log("I run only once")}, []);
-  useEffect(()=>{
-      console.log("I run when 'keyword' changes");
-  }, [keyword]);
-  useEffect(()=>{
-      console.log("I run when 'counter' changes");
-  }, [counter]);
-  useEffect(()=>{
-      console.log("I run when 'keyword & counter' changes");
-  }, [keyword, counter]);
-  
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."></input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click</button>
+      <button onClick={onClick}>{showing ? "Hide":"Show"}</button>
+      {showing ? <Hello /> : null}
     </div>
   );
 }
