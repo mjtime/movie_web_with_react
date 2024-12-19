963261cd12e51b7d1f032eb62381b4a63cb5127e
mjtime <min021802@gmail.com>
2024-12-20 01:21:19 +0900

    [7.0] to do list 만들기1

--- react-for-beginners2/src/App.js
+++ react-for-beginners2/src/App.js
@@ -1,20 +1,25 @@
-import { useState, useEffect } from "react";
-
-function Hello(){
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  }, []);
-  return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {  // toDo input이 비었다면 작동하지 않음
+      return;
+    }
+    setToDos(currentArray => [toDo, ...currentArray]);
+    setToDo("");  // submit 후 ToDo input 공백으로 비우기
+  }
+  console.log(toDos);
   return (
     <div>
-      <button onClick={onClick}>{showing ? "Hide":"Show"}</button>
-      {showing ? <Hello /> : null}
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input value={toDo} onChange={onChange} type="text" placeholder="Write your to do..."/>
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
