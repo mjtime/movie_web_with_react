f4d6c788c7d9336beac8f95e64db72eedc0029b1
mjtime <min021802@gmail.com>
2024-12-22 01:54:23 +0900

    [7.1] to do list 만들기2_리스트로 출력

--- react-for-beginners2/src/App.js
+++ react-for-beginners2/src/App.js
@@ -12,7 +12,7 @@ function App() {
     setToDos(currentArray => [toDo, ...currentArray]);
     setToDo("");  // submit 후 ToDo input 공백으로 비우기
   }
-  console.log(toDos);
+
   return (
     <div>
       <h1>My To Dos ({toDos.length})</h1>
@@ -20,6 +20,12 @@ function App() {
         <input value={toDo} onChange={onChange} type="text" placeholder="Write your to do..."/>
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key = {index}>{item}</li>
+          ))}
+      </ul>
     </div>
   );
 }
