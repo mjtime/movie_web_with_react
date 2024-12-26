1b982b2fffdd13fd60e9a63ce43677bcc1958f6c
mjtime <min021802@gmail.com>
2024-12-27 00:15:51 +0900

    [7.3] movie app 영화 정보 불러오고 화면에 출력하기

--- react-for-beginners2/src/App.js
+++ react-for-beginners2/src/App.js
@@ -2,28 +2,45 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [conins, setCoins] = useState([]);
-  useEffect(()=>{
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+      )
+    ).json();
 
-  }, [])
-  return(
-  <div>
-    <h1>The Coins! {loading ? "" : `(${conins.length})`}</h1>
-    {loading ? <strong>Loading...</strong>:(<select>
-      
-      {conins.map((coin) => (
-        <option key={coin.id}>{coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD</option>
-      ))};   
-    </select>)}
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
 
-  </div>
-);
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  console.log(movies);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image}></img>
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
