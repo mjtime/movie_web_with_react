a399f4ca37afc387356b84e36e59fe72424f58e4
mjtime <min021802@gmail.com>
2025-01-24 01:31:21 +0900

    feat: 영화 검색창 클리어 버튼 기능 추가 및 검색창 확인용 css 적용

--- react-for-beginners2/src/routes/Home.js
+++ react-for-beginners2/src/routes/Home.js
@@ -1,11 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Movie from "../components/Movie";
 import { useSearchParams } from "react-router-dom";
+import styles from "./Home.module.css";
 
 function Home() {
   const [loading, setLoading] = useState(true); // 로딩 상태
   const [movies, setMovies] = useState([]); // 영화 정보
   const [searchParams, setSearchParams] = useSearchParams();
+  const searchInputRef = useRef(null);
 
   // 장르 정보 하드코딩
   const genre_list = [
@@ -38,7 +40,11 @@ function Home() {
     "Western",
   ];
 
-  const getMovies = async () => {
+  // **메인페이지에 기본적**으로 출력할 장르별 영화들 요청
+  const getMovieByGenres = async () => {};
+
+  // **장르, 영화 제목 검색시** 영화 정보들 출력
+  const getFilteredMovies = async () => {
     // url 객체 생성
     const url = new URL("https://yts.mx/api/v2/list_movies.json?");
 
@@ -72,8 +78,8 @@ function Home() {
   };
 
   useEffect(() => {
-    getMovies();
-  }, [searchParams]); // searchParams가 변경될 때마다 getMovies 호출
+    getFilteredMovies();
+  }, [searchParams]); // searchParams가 변경될 때마다 getFilteredMovies 호출
 
   // SearchParams에 장르 카테고리 변경사항 적용
   const handleGenreChange = (genre) => {
@@ -93,12 +99,24 @@ function Home() {
     setSearchParams((prevParams) => {
       const newParams = new URLSearchParams(prevParams);
       if (query) {
-        newParams.set("query_term", query);
+        newParams.set("query_term", query); // 검색어에서 기존에 입력된 내용 제거
       } else {
         newParams.delete("query_term"); // 검색어 없을시 제거
       }
       return newParams;
     });
+  };
+
+  // 검색창 clear 버튼
+  const clearSearch = () => {
+    setSearchParams((prevParams) => {
+      const newParams = new URLSearchParams(prevParams);
+      newParams.delete("query_term"); // 검색어에서 기존에 입력된 내용 제거
+      return newParams;
+    });
+    if (searchInputRef.current) {
+      searchInputRef.current.value = ""; // input에 남아있는 value 제거
+    }
   };
 
   return (
@@ -120,14 +138,25 @@ function Home() {
             </ul>
           </div>
           {/*제목 검색창, onSubmit 새로고침 방지, 실시간 검색*/}
-          <form onSubmit={(e) => e.preventDefault()}>
+          <form
+            onSubmit={(e) => e.preventDefault()}
+            className={styles.searchContainer}
+          >
             <input
               type="text"
-              nams="query_term"
+              name="query_term"
               placeholder="movie title"
               defaultValue={searchParams.get("query_term") || ""}
               onChange={(e) => handleSearch(e.target.value)}
+              ref={searchInputRef}
+              className={styles.searchInput}
             />
+            {/* clear button, 검색어가 존재할 시에만 활성화 */}
+            {searchInputRef.current && searchInputRef.current.value && (
+              <button onClick={clearSearch} className={styles.clearButton}>
+                X
+              </button>
+            )}
           </form>
           {/* 영화 목록 */}
           {movies.length > 0 ? (
