4c1f04bc2d08748dc3815c3acf6667f573ba0c5f
mjtime <min021802@gmail.com>
2025-06-04 00:40:43 +0900

    feat(storybook): NoPoster 컴포넌트 스토리 작성

---
+++ react-for-beginners2/src/components/NoPoster/NoPoster.stories.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NoPoster from "./NoPoster";
+
+// 1) 기본 export: 메타 정보
+export default {
+  title: "Components/NoPoster", // 사이드바에 표시될 위치
+  component: NoPoster, // 이 스토리가 다룰 컴포넌트
+};
+
+// 2) 이름 있는 export: 스토리
+export const Default = () => <NoPoster title="No Image Available" />;
