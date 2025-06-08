222fd1db9a12637c6b2d9a798072d05ce609de33
mjtime <min021802@gmail.com>
2025-06-09 02:13:07 +0900

    refactor(storybook): SlideShow 스토리 공통 loaders & render 헬퍼 도입

--- react-for-beginners2/src/components/SlideShow/SlideShow.stories.jsx
+++ react-for-beginners2/src/components/SlideShow/SlideShow.stories.jsx
@@ -3,46 +3,33 @@ import React from "react";
 import { MemoryRouter } from "react-router-dom";
 import SlideShow from "./SlideShow";
 
+const createLoader = (url) => [
+  async () => {
+    const res = await fetch(url);
+    const json = await res.json();
+    return { movies: json.data.movies };
+  },
+];
+const renderWithMovies = (args, { loaded: { movies } }) => (
+  <SlideShow {...args} movies={movies} />
+);
+
 export default {
   title: "Components/SlideShow",
   component: SlideShow,
   decorators: [(storyFn) => <MemoryRouter>{storyFn()}</MemoryRouter>],
 };
 
-// Default 스토리
 export const Default = {
-  loaders: [
-    async () => {
-      const res = await fetch(
-        "https://yts.mx/api/v2/list_movies.json?sort_by=rating&limit=5"
-      );
-      const json = await res.json();
-      return { movies: json.data.movies };
-    },
-  ],
-  args: {
-    movies: [], // 로더 실행 전 기본값
-  },
-  render: (args, { loaded: { movies } }) => (
-    <SlideShow {...args} movies={movies} />
+  loaders: createLoader(
+    "https://yts.mx/api/v2/list_movies.json?sort_by=rating&limit=5"
   ),
+  args: { movies: [] },
+  render: renderWithMovies,
 };
 
-// NoImages 스토리 (포스터, 배경 이미지 없는 경우)
 export const NoImages = {
-  loaders: [
-    async () => {
-      const res = await fetch(
-        "https://yts.mx/api/v2/list_movies_no_images.json"
-      );
-      const json = await res.json();
-      return { movies: json.data.movies };
-    },
-  ],
-  args: {
-    movies: [],
-  },
-  render: (args, { loaded: { movies } }) => (
-    <SlideShow {...args} movies={movies} />
-  ),
+  loaders: createLoader("https://yts.mx/api/v2/list_movies_no_images.json"),
+  args: { movies: [] },
+  render: renderWithMovies,
 };
