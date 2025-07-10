c49beaa2e30173f36095757cb086a915b60b7e5b
mjtime <min021802@gmail.com>
2025-07-11 02:56:40 +0900

    chore(mock): list_movies_12.json 요청에 대한 mock 핸들러 추가
    - 테스트용 영화 12편 구성 (이미지 있는 경우, 없는 경우 포함)

--- react-for-beginners2/src/mocks/handlers.js
+++ react-for-beginners2/src/mocks/handlers.js
@@ -62,4 +62,112 @@ export const handlers = [
       },
     })
   ),
+
+  // 영화 정보 12개
+  http.get(/https:\/\/yts\.mx\/api\/v2\/list_movies_12\.json.*/, () =>
+    HttpResponse.json({
+      data: {
+        movies: [
+          {
+            id: 1,
+            title: "Movie title 1",
+            background_image: "mock-images/bg1.jpg",
+            medium_cover_image: "mock-images/poster1.jpg",
+            summary:
+              "영화 요약 공간 영화 요약 공간 영화 요약 공간 영화 요약 공간 영화 요약 공간 영화 요약 공간 영화 요약 공간 영화 요약 공간 영화 요약 공간 영화 요약 공간 영화 요약 공간 영화 요약 공간 영화 요약 공간 ",
+            rating: 0.1,
+          },
+          {
+            id: 2,
+            title: "Movie title 2",
+            background_image: "mock-images/bg2.jpg",
+            medium_cover_image: "mock-images/poster2.jpg",
+            summary: "",
+            rating: 8.7,
+          },
+          {
+            id: 3,
+            title: "123456789012345678901234567890",
+            background_image: null,
+            medium_cover_image: null,
+            summary: "포스터/배경 없는 영화 1 - null 값인 경우",
+            rating: 5.5,
+          },
+          {
+            id: 4,
+            title: "Movie title 3",
+            background_image: "mock-images/bg3.jpg",
+            medium_cover_image: "mock-images/poster3.jpg",
+            summary: "요약",
+            rating: 10.0,
+          },
+
+          {
+            id: 5,
+            title: "Movie title 1",
+            background_image: "mock-images/bg1.jpg",
+            medium_cover_image: "mock-images/poster1.jpg",
+            summary: "",
+            rating: 0.1,
+          },
+          {
+            id: 6,
+            title: "Movie title 2",
+            background_image: "mock-images/bg2.jpg",
+            medium_cover_image: "mock-images/poster2.jpg",
+            summary: "",
+            rating: 8.7,
+          },
+          {
+            id: 7,
+            title: "Movie title 3",
+            background_image: "mock-images/bg3.jpg",
+            medium_cover_image: "mock-images/poster3.jpg",
+            summary: "요약",
+            rating: 10.0,
+          },
+          {
+            id: 8,
+            title: "Movie title 1",
+            background_image: "mock-images/bg1.jpg",
+            medium_cover_image: "mock-images/poster1.jpg",
+            summary: "",
+            rating: 0.1,
+          },
+          {
+            id: 9,
+            title: "Movie title 2",
+            background_image: "mock-images/bg2.jpg",
+            medium_cover_image: "mock-images/poster2.jpg",
+            summary: "",
+            rating: 8.7,
+          },
+          {
+            id: 10,
+            title: "Movie title 3",
+            background_image: "mock-images/bg3.jpg",
+            medium_cover_image: "mock-images/poster3.jpg",
+            summary: "요약",
+            rating: 10.0,
+          },
+          {
+            id: 11,
+            title: "Movie without images 2",
+            background_image: "",
+            medium_cover_image: "",
+            summary: "포스터/배경 없는 영화 2 - 빈 문자열인 경우",
+            rating: 7.0,
+          },
+          {
+            id: 12,
+            title: "Movie title 3",
+            background_image: "mock-images/bg3.jpg",
+            medium_cover_image: "mock-images/poster3.jpg",
+            summary: "요약",
+            rating: 10.0,
+          },
+        ],
+      },
+    })
+  ),
 ];
